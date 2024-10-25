8cde40c3e26c7ed8a3f4f8dcd7299489624717e8
pebencze <petra.bncz@gmail.com>
2024-10-25 13:06:00 +0200

    changed main

--- src/main.c
+++ src/main.c
@@ -1,9 +1,15 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include "../incl/minirt.h"
 
 int main(void)
 {
+	t_data *data;
+
+	data->mlx_ptr = mlx_init();
+	if (data->mlx_ptr == )
+
 	printf("hello world");
 	return (0);
 }
