bf86ab81eb854562e7a5ac3c7c1a087a9dc5743a
Vanessa Dos Santos <vados-sa@c3a1c6.42berlin.de>
2024-11-01 18:37:33 +0100

    draw_background

--- src/draw_background.c
+++ src/draw_background.c
@@ -1,18 +1,9 @@
 
 #include "minirt.h"
 
-void	our_put_pixel(mlx_image_t* image, uint32_t x, uint32_t y, uint32_t color)
+uint32_t	create_color(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
 {
-	uint8_t*	pixel;
-
-	//MLX_ASSERT(x < image->width, "Pixel is out of bounds");
-	//MLX_ASSERT(y < image->height, "Pixel is out of bounds");
-
-	pixel = &image->pixels[(y * image->width + x) * sizeof(int32_t)];
-	*(pixel++) = (uint8_t)((color >> 24));
-	*(pixel++) = (uint8_t)((color >> 16));
-	*(pixel++) = (uint8_t)((color >> 8));
-	*(pixel++) = (uint8_t)(color & 0xFF);
+	return (b << 24 | g << 16 | r << 8 | a);
 }
 
 void	draw_background(t_data *data)
@@ -22,14 +13,13 @@ void	draw_background(t_data *data)
 	uint32_t	color;
 
 	y = 0;
-	color = 16711680;
-	our_put_pixel(data->image, 600, 500, color);
-	while (y < (uint32_t)IMAGE_HEIGHT)
+	color = create_color(94, 4, 3, 255);
+	while (y < IMAGE_HEIGHT)
 	{
 		x = 0;
-		while (x < (uint32_t)IMAGE_WIDTH)
+		while (x < IMAGE_WIDTH)
 		{
-			our_put_pixel(data->image, x, y, color);
+			mlx_put_pixel(data->image, x, y, color);
 			x++;
 		}
 		y++;
