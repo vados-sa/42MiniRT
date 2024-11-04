51118bf452db0e233bfd88e7d874d30ef6979c7c
pebencze <petra.bncz@gmail.com>
2024-11-04 17:50:12 +0100

    fixed camera up and right

--- src/main.c
+++ src/main.c
@@ -19,8 +19,8 @@ void	setup_viewport(t_data *data, t_C camera)
 		world_up = coord(0.0, 0.0, 1.0);
 	data->vp.width = 2.0 * tan((camera.fov * PI / 180.0) / 2.0);
 	data->vp.height = data->vp.width / ((double)IMAGE_WIDTH / (double)IMAGE_HEIGHT);
-	camera_right = vec_unit(vec_cross(world_up, camera.orientation));
-	camera_up = vec_unit(vec_cross(camera.orientation, camera_right));
+	camera_right = vec_unit(vec_cross(camera.orientation, world_up));
+	camera_up = vec_unit(vec_cross(camera_right, camera.orientation));
 	data->vp.center = vec_add(camera.center, vec_mult(camera.orientation, \
 						FOCAL_LENGTH));
 	data->vp.up_left = vec_add(vec_sub(data->vp.center, \
@@ -34,6 +34,15 @@ void	setup_viewport(t_data *data, t_C camera)
 	vec_mult((vec_add(data->vp.pixel_x, data->vp.pixel_y)), 0.5));
 }
 
+void print_vp(t_vp vp) {
+    printf("\nViewport Width: %.15lf\n", vp.width);
+    printf("Viewport Height: %.15lf\n", vp.height);
+    printf("Viewport Center: (%.15lf, %.15lf, %.15lf)\n", vp.center.x, vp.center.y, vp.center.z);
+    printf("Viewport Up Left: (%.15lf, %.15lf, %.15lf)\n", vp.up_left.x, vp.up_left.y, vp.up_left.z);
+    printf("Pixel X: (%.15lf, %.15lf, %.15lf)\n", vp.pixel_x.x, vp.pixel_x.y, vp.pixel_x.z);
+    printf("Pixel Y: (%.15lf, %.15lf, %.15lf)\n", vp.pixel_y.x, vp.pixel_y.y, vp.pixel_y.z);
+    printf("Pixel 00: (%.15lf, %.15lf, %.15lf)\n", vp.pixel00.x, vp.pixel00.y, vp.pixel00.z);
+}
 
 int	main(int argc, char **argv)
 {
@@ -45,6 +54,7 @@ int	main(int argc, char **argv)
 	parse(data, argv[1]);
 	draw_background(data);
 	setup_viewport(data, data->scene->c);
+	print_vp(data->vp);
 	//mlx_loop_hook(data->mlx_ptr, &draw_background, data);
 	//mlx_loop_hook(data->mlx_ptr, &render, data);
 	mlx_key_hook(data->mlx_ptr, (t_mlx_keyfunc)my_keyhook, data);
