42a84cb6bfe20dce15b7b66ca8dc2d90e2a58e11
pebencze <petra.bncz@gmail.com>
2024-11-04 18:04:54 +0100

    took out test

--- src/main.c
+++ src/main.c
@@ -34,16 +34,6 @@ void	setup_viewport(t_data *data, t_C camera)
 	vec_mult((vec_add(data->vp.pixel_x, data->vp.pixel_y)), 0.5));
 }
 
-void print_vp(t_vp vp) {
-    printf("\nViewport Width: %.15lf\n", vp.width);
-    printf("Viewport Height: %.15lf\n", vp.height);
-    printf("Viewport Center: (%.15lf, %.15lf, %.15lf)\n", vp.center.x, vp.center.y, vp.center.z);
-    printf("Viewport Up Left: (%.15lf, %.15lf, %.15lf)\n", vp.up_left.x, vp.up_left.y, vp.up_left.z);
-    printf("Pixel X: (%.15lf, %.15lf, %.15lf)\n", vp.pixel_x.x, vp.pixel_x.y, vp.pixel_x.z);
-    printf("Pixel Y: (%.15lf, %.15lf, %.15lf)\n", vp.pixel_y.x, vp.pixel_y.y, vp.pixel_y.z);
-    printf("Pixel 00: (%.15lf, %.15lf, %.15lf)\n", vp.pixel00.x, vp.pixel00.y, vp.pixel00.z);
-}
-
 int	main(int argc, char **argv)
 {
 	t_data	*data;
@@ -54,12 +44,10 @@ int	main(int argc, char **argv)
 	parse(data, argv[1]);
 	draw_background(data);
 	setup_viewport(data, data->scene->c);
-	print_vp(data->vp);
 	//mlx_loop_hook(data->mlx_ptr, &draw_background, data);
 	//mlx_loop_hook(data->mlx_ptr, &render, data);
 	mlx_key_hook(data->mlx_ptr, (t_mlx_keyfunc)my_keyhook, data);
 	mlx_loop(data->mlx_ptr);
-	//print_data(data);
 	free_data(data);
 	return (0);
 }
