2f59dc57abfe471c731cc3c4e58231b77c838fd6
pebencze <petra.bncz@gmail.com>
2024-11-06 13:55:10 +0100

    changed strdup

--- lib/libft/ft_strdup.c
+++ lib/libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: pbencze <pbencze@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 18:37:04 by pbencze           #+#    #+#             */
-/*   Updated: 2024/02/02 15:35:38 by pbencze          ###   ########.fr       */
+/*   Updated: 2024/11/06 13:47:04 by pbencze          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,19 @@ char	*ft_strdup(const char *s)
 {
 	char	*duplicate;
 	size_t	size;
+	int		i;
 
 	size = (ft_strlen(s) + 1);
 	duplicate = malloc(size * sizeof(char));
 	if (!duplicate)
 		return (NULL);
-	ft_strlcpy(duplicate, s, size);
+	i = 0;
+	while (s[i])
+	{
+		duplicate[i] = s[i];
+		i++;
+	}
+	duplicate[i] = '\0';
 	return (duplicate);
 }
 
