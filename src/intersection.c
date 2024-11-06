ea1a1f14047f80e97da14c7024639b5ddd00109f
Vanessa Dos Santos <vados-sa@c4b7c2.42berlin.de>
2024-11-06 18:36:17 +0100

    compare distance of objects and changed sphere intersection function

--- src/intersection.c
+++ src/intersection.c
@@ -1,6 +1,60 @@
 
 #include "minirt.h"
 
+t_intersec	*compare_distance(t_intersec *obj_1, t_intersec *obj_2, \
+								t_coord origin)
+{
+	t_float	d1;
+	t_float	d2;
+
+	if (!obj_2)
+		return(obj_1);
+	d1 = sqrt(pow((obj_1->point.x - origin.x), 2) +
+				pow((obj_1->point.y - origin.y), 2) +
+				pow((obj_1->point.z - origin.z), 2));
+	d2 = sqrt(pow((obj_2->point.x - origin.x), 2) +
+				pow((obj_2->point.y - origin.y), 2) +
+				pow((obj_2->point.z - origin.z), 2));
+	if (d1 <= d2)
+		return (obj_1);
+	else
+		return (obj_2);
+}
+
+t_intersec	*sphere_intersect(t_data *data, t_ray ray, t_object *obj)
+{
+	t_float	discriminant;
+	t_float	a;
+	t_float	b;
+	t_float	c;
+	t_coord	oc;
+	t_float	t_min;
+	t_float	t_max;
+
+	oc = vec_sub(obj->sp.center, ray.origin);
+	a = vec_dot(ray.direction, ray.direction);
+	b = -2.0 * vec_dot(ray.direction, oc);
+	c = vec_dot(oc, oc) - (obj->sp.radius * obj->sp.radius);
+	discriminant = (b * b) - (4 * a * c);
+	if (discriminant >= 0)
+	{
+		t_min = (-b - (sqrt(discriminant)) / 2 * a);
+		t_max = (-b + (sqrt(discriminant)) / 2 * a);
+		if (t_min >= 0)
+			obj->temp.t = t_min; // sphere in front of the camera
+		else if (t_max >= 0)
+			obj->temp.t = t_max; // camera inside sphere
+		else
+			return (NULL);
+		obj->temp.point = ray_at(ray, obj->temp.t);
+		//obj->temp.normal = find_normal();
+		obj->temp.color = obj->sp.color;
+		return (&obj->temp);
+	}
+	else
+		return (NULL);
+}
+
 int	intersection(t_data *data, t_ray ray, uint32_t x, uint32_t y)
 {
 	t_object	*object;
@@ -18,12 +72,13 @@ int	intersection(t_data *data, t_ray ray, uint32_t x, uint32_t y)
 			temp = plane_intersect();
 		else if (object->type == 'c')
 			temp = cylinder_intersect();
-		closest = compare_records(temp, closest);
+		closest = compare_distance(temp, closest, ray.origin);
 		object = object->next;
 	}
 	if (closest)
 		return (put_color(closest->color, x, y, data), 1);
 		//calculate the color based on the lights and the object
 		//return the color
+		//free(closest);
 	return (0);
 }
