a49a49259835b2f981e2a0d773e152199b16ffd6
pebencze <petra.bncz@gmail.com>
2024-11-07 13:00:43 +0100

    added some .rt files

--- src/plane.c
+++ src/plane.c
@@ -6,29 +6,31 @@ t_coord	ray_at(t_ray r, t_float t)
 	return (vec_add(r.origin, vec_mult(r.direction, t)));
 }
 
+/*If the plane and ray are parallel (i.e., when approaches 0),
+they either perfectly coincide, offering an infinite number of solutions,
+or they do not intersect at all. In practical C++ implementations,
+we typically return false (indicating no intersection)
+when the denominator is less than a very small threshold.*/
 t_intersec	*plane_intersect(t_ray ray, t_object *obj)
 {
 	t_float		numerator;
 	t_float		denominator;
 
-	numerator = vec_dot(vec_sub(ray.origin,obj->pl.point), obj->pl.normal);
-	denominator = vec_dot(ray.direction, obj->pl.normal);
-	if (numerator == 0 && denominator == 0)
+	numerator = vec_dot(vec_sub(obj->pl.point, ray.origin), obj->pl.normal);
+	denominator = vec_dot(obj->pl.normal, ray.direction);
+	if (numerator == 0 && denominator == 0) //no intersection, parallel plane
 		return (NULL);
-	else if (denominator == 0)
-	{
-		//? ray is inside of the plane
-		obj->temp.color = obj->pl.color;
-		obj->temp.t = 0;
-		obj->temp.point = ray.origin;
-		obj->temp.normal = obj->pl.normal;
-	}
+	else if (denominator < 1e-6) //infinite intersections, ray inside plane
+		return (NULL);
 	else
 	{
 		obj->temp.color = obj->pl.color;
 		obj->temp.t = numerator / denominator;
 		obj->temp.point = ray_at(ray, obj->temp.t);
-		obj->temp.normal = obj->pl.normal; // or is it something else?
+		if (denominator > 0)
+			obj->temp.normal = vec_mult(obj->pl.normal, -1.0); //if ray intersects from the back, invert normal
+		else
+			obj->temp.normal = obj->pl.normal;
 	}
 	return (&obj->temp);
 }
