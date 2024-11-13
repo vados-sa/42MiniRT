7107190ac7a5c8da0843ddf8b8519286161a9087
pebencze <petra.bncz@gmail.com>
2024-11-13 13:51:49 +0100

    added another version of check_shadow in comments

--- src/lights.c
+++ src/lights.c
@@ -39,23 +39,24 @@ t_color	ambient_light(t_A ambient)
  * @param l: a linked list of light sources
  * @param hit_rec: a t_intersection struct that contains the surface normal and the point where the object was hit
  */
-t_color	diffuse_light(t_L *l, t_intersec *hit_rec)
+t_color	diffuse_light(t_L *l, t_intersec *hit_rec, t_data *data)
 {
-	t_coord	light_dir;
+	t_ray	light;
 	t_float	dot_product;
 	t_color	diffuse;
 	t_color	temp_diffuse;
-	//bool	shadow;
+	bool	shadow;
 
 	diffuse = col(0, 0, 0, 0);
 	while (l && hit_rec)
 	{
 		//figure out how to combine multiple lights
 		//hit_rec->normal = vec_unit(hit_rec->normal); //do we need it?
-		light_dir = vec_unit(vec_sub(hit_rec->point, l->point));
-		dot_product = fmax(vec_dot(hit_rec->normal, light_dir), 0.0); //if angle is bigger than 90deg, no light
-		//shadow = check_shadow();
-		if (dot_product > 0.0) //&& shadow == false)
+		light.direction = vec_unit(vec_sub(hit_rec->point, l->point));
+		light.origin = l->point;
+		dot_product = fmax(vec_dot(hit_rec->normal, light.direction), 0.0); //if angle is bigger than 90deg, no light
+		shadow = check_shadow(light, hit_rec->point, data);
+		if (dot_product > 0.0 && shadow == false)
 		{
 			temp_diffuse = col_mult(col_mult(l->color, dot_product), l->brightness);
 			diffuse = col_add(diffuse, temp_diffuse);
@@ -72,7 +73,7 @@ t_color	calculate_light(t_data *data, t_intersec *hit_rec, t_color color)
 	t_color	diffuse;
 
 	ambient = ambient_light(data->scene->a);
-	diffuse = diffuse_light(data->scene->l, hit_rec);
+	diffuse = diffuse_light(data->scene->l, hit_rec, data);
 	result.r = ((t_float)(ambient.r + diffuse.r) / 255.0) * color.r;
 	result.g = ((t_float)(ambient.g + diffuse.g) / 255.0) * color.g;
 	result.b = ((t_float)(ambient.b + diffuse.b) / 255.0) * color.b;
@@ -85,3 +86,42 @@ t_color	calculate_light(t_data *data, t_intersec *hit_rec, t_color color)
 	result.a = 255;
 	return (result);
 }
+
+bool	check_shadow(t_ray light, t_coord origin, t_data *data)
+{
+	t_intersec	*shadow_hit;
+	t_ray		shadow_ray;
+
+	shadow_ray.direction = vec_mult(light.direction, -1.0);
+	shadow_ray.origin = origin;
+	shadow_hit = intersection(data, shadow_ray);
+	if (shadow_hit && shadow_hit->t > 1e-6
+		&& shadow_hit->t < vec_len(vec_sub(light.origin, shadow_ray.origin)))
+		return (true);
+	return (false);
+}
+/* bool	check_shadow(t_ray light, t_coord origin, t_data *data)
+{
+	t_intersec	*shadow_hit;
+	t_object	*object;
+	t_ray		shadow_ray;
+
+	shadow_ray.direction = vec_mult(light.direction, -1.0);
+	shadow_ray.origin = origin;
+	object = data->scene->objects;
+	while (object)
+	{
+		shadow_hit = NULL;
+		if (object->type == 's')
+			shadow_hit = sphere_intersect(data, shadow_ray, object);
+		else if (object->type == 'p')
+			shadow_hit = plane_intersect(shadow_ray, object);
+		else if (object->type == 'c')
+			shadow_hit = cylinder_intersect(shadow_ray, object);
+		object = object->next;
+		if (shadow_hit && shadow_hit->t > 1e-6
+			&& shadow_hit->t < vec_len(vec_sub(light.origin, shadow_ray.origin)))
+			return (true);
+	}
+	return (false);
+} */
