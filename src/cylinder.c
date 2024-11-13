2c3ff1242f7cd81acecae1aa00f625738dc2e1c5
Vanessa Dos Santos <vados-sa@c3a1c6.42berlin.de>
2024-11-13 14:32:01 +0100

    surfice normal vector

--- src/cylinder.c
+++ src/cylinder.c
@@ -1,12 +1,12 @@
 
 #include "../incl/minirt.h"
 
-/* t_coord local_normal_at(t_coord point, t_object *obj, t_float m)
+t_coord local_normal_at(t_coord point, t_object *obj, t_float m)
 {
     t_coord normal;
     normal = vec_sub(point, vec_add(obj->cy.center, vec_mult(obj->cy.normal, m)));
     return vec_unit(normal);
-} */
+}
 
 static t_float find_discriminant(t_ray ray, t_object *obj, t_float a, t_float b)
 {
@@ -62,61 +62,6 @@ t_intersec *cylinder_intersect(t_ray ray, t_object *obj)
     obj->temp.t = t;
 	obj->temp.point = ray_at(ray, obj->temp.t);
 	obj->temp.color = obj->cy.color;
-    //intersec.normal = local_normal_at(point, obj, m);
+    obj->temp.normal = local_normal_at(obj->temp.point, obj, m);
     return (&obj->temp);
 }
-
-/* t_intersec *cylinder_intersect(t_ray ray, t_object *obj) // copilot version
-{
-    t_float a, b, c, discriminant, t1, t2, t;
-    t_coord point;
-    t_intersec intersec;
-    t_coord X = vec_sub(ray.origin, obj->cy.top_end_cap);
-    t_float D_dot_V = vec_dot(ray.direction, obj->cy.normal);
-    t_float X_dot_V = vec_dot(X, obj->cy.normal);
-
-    // Calculate the coefficients of the quadratic equation
-    a = vec_dot(ray.direction, ray.direction) - D_dot_V * D_dot_V;
-    b = 2 * (vec_dot(ray.direction, X) - D_dot_V * X_dot_V);
-    c = vec_dot(X, X) - X_dot_V * X_dot_V - obj->cy.radius * obj->cy.radius;
-
-    discriminant = b * b - 4 * a * c;
-    if (discriminant < 0)
-        return NULL;
-
-    t1 = (-b - sqrt(discriminant)) / (2.0 * a);
-    t2 = (-b + sqrt(discriminant)) / (2.0 * a);
-
-    if (t1 > t2)
-    {
-        t_float temp = t1;
-        t1 = t2;
-        t2 = temp;
-    }
-
-    // Check if the closest intersection point is within the cylinder's height
-    t_float m1 = D_dot_V * t1 + X_dot_V;
-    t_float m2 = D_dot_V * t2 + X_dot_V;
-
-    if (t1 >= 0 && m1 >= 0 && m1 <= obj->cy.height)
-    {
-        t = t1;
-    }
-    else if (t2 >= 0 && m2 >= 0 && m2 <= obj->cy.height)
-    {
-        t = t2;
-    }
-    else
-    {
-        return NULL;
-    }
-
-    point = ray_at(ray, t);
-    intersec.t = t;
-    intersec.point = point;
-    //intersec.normal = local_normal_at(point, obj, (t == t1) ? m1 : m2);
-    intersec.color = obj->cy.color;
-
-    obj->temp = intersec; // Store the intersection in the object's temp field
-    return (&obj->temp);
-} */
