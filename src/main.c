4381656a9c18d24f2422570a8df7446de5be6b37
pebencze <petra.bncz@gmail.com>
2024-11-20 11:32:48 +0100

    hooks are working, but plane has shadows on the other side

--- src/main.c
+++ src/main.c
@@ -11,15 +11,14 @@ void	my_keyhook(mlx_key_data_t keydata, t_data *data)
 		mlx_close_window(data->mlx_ptr);
 		//ft_exit(0, data, NULL);
 	if (keydata.key == MLX_KEY_UP && keydata.action == MLX_PRESS)
-		data->scene->c.center = vec_sub(camera.center, vec_mult(camera.up, 0.01));
+		data->scene->c.center = vec_add(camera.center, vec_mult(camera.up, 0.1));
 	if (keydata.key == MLX_KEY_DOWN && keydata.action == MLX_PRESS)
-		data->scene->c.center = vec_add(camera.center, vec_mult(camera.up, 0.01));
+		data->scene->c.center = vec_sub(camera.center, vec_mult(camera.up, 0.1));
 	if (keydata.key == MLX_KEY_RIGHT && keydata.action == MLX_PRESS)
-		data->scene->c.center = vec_sub(camera.center, vec_mult(camera.right, 0.01));
+		data->scene->c.center = vec_add(camera.center, vec_mult(camera.right, 0.1));
 	if (keydata.key == MLX_KEY_LEFT && keydata.action == MLX_PRESS)
-		data->scene->c.center = vec_add(camera.center, vec_mult(camera.right, 0.01));
-	//setup_viewport(data, data->scene->c); //if I add this, it rerenders the objects correctly, othrwise it moves around in a ready image
-	//render(data);
+		data->scene->c.center = vec_sub(camera.center, vec_mult(camera.right, 0.1));
+	setup_viewport(data, data->scene->c); //if I add this, it rerenders the objects correctly, othrwise it moves around in a ready image
 }
 
 void	resize(int32_t width, int32_t height, t_data *data)
@@ -43,7 +42,6 @@ int	main(int argc, char **argv)
 	init(&data);
 	parse(data, argv[1]);
 	setup_viewport(data, data->scene->c);
-	//render(data);
 	mlx_loop_hook(data->mlx_ptr, (t_hookfunc)render, data);
 	mlx_resize_hook(data->mlx_ptr, (t_resizefunc)resize, data);
 	mlx_key_hook(data->mlx_ptr, (t_keyfunc)my_keyhook, data);
