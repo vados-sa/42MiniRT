81346d4dfdda6be63d4a5d5bf8d564fd0942ba95
Vanessa Dos Santos <vados-sa@c3a1c4.42berlin.de>
2024-12-02 16:34:57 +0100

    made error messages prettier

--- incl/macros.h
+++ incl/macros.h
@@ -3,22 +3,24 @@
 # define MACROS_H
 
 //error messages
-# define ALLOC_ERR "Error\nerror on allocation"
-# define ARGV_ERR "Error\nusage: ./minirt *.rt"
-# define OPEN_ERR "Error\nfile does not exist or cannot be opened"
-# define EXTENSION_ERR "Error\nformat *.rt required"
-# define EMPTY_ERR "Error\nempty file"
-# define TYPE_ID_ERR "Error\nspace at the beginning of the line or \
-type identifier is not valid or is declared more than once"
-# define SPACE_ERR "Error\nspace at the end of the line"
-# define LEN_ERR "Error\nelement doesn't contain the right amount of information"
-# define RATIO_ERR "Error\nlight ratio invalid or out of bounds"
-# define FOV_ERR "Error\nfov invalid or out of bounds"
-# define GEN_INFO_ERR "Error\nwrong amount of information"
-# define COL_RANGE_ERR "Error\nrgba values are out of bounds"
-# define NORMAL_RANGE_ERR "Error\nnormal values are out of bounds"
-# define NUM_ERR "Error\nnumber contains invalid characters"
-# define DIMENSION_ERR "Error\nobject dimensions have to be positive values"
+# define ALLOC_ERR "Error\nAllocation error."
+# define ARGV_ERR "Error\nUsage: ./minirt *.rt"
+# define OPEN_ERR "Error\nFile does not exist or cannot be opened."
+# define EXTENSION_ERR "Error\nFormat *.rt required."
+# define EMPTY_ERR "Error\nEmpty file."
+# define TYPE_ID_ERR "Error\nType identifier is not valid, or is declared \
+more than once, or space at the beginning of the line."
+# define SPACE_ERR "Error\nSpace at the end of the line."
+# define LEN_ERR "Error\nElement doesn't contain the right amount of \
+information."
+# define RATIO_ERR "Error\nLight ratio invalid or out of bounds."
+# define FOV_ERR "Error\nFOV invalid or out of bounds."
+# define GEN_INFO_ERR "Error\nWrong amount of information."
+# define COL_RANGE_ERR "Error\nR,G,B color values are out of bounds."
+# define NORMAL_RANGE_ERR "Error\n3d normalized orientation vector's \
+values are out of bounds."
+# define NUM_ERR "Error\nNumber contains invalid characters."
+# define DIMENSION_ERR "Error\nObject dimensions have to be positive values."
 
 //macros
 # define IMAGE_WIDTH 1200.0
