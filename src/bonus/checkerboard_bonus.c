6c015716b672434439804b3dd0a958556236ddb9
pebencze <petra.bncz@gmail.com>
2024-12-03 16:48:30 +0100

    checkerboard works'

--- src/bonus/checkerboard_bonus.c
+++ src/bonus/checkerboard_bonus.c
@@ -6,17 +6,17 @@ t_coord	planar_map(t_coord plane, t_coord normal)
 	t_float	u;
 	t_float	v;
 
-	if (fabs(normal.y) > EPSILON) //doublecheck this before eval!!
+	if (fabs(normal.y) > EPSILON)
 	{
 		u = plane.x - floor(plane.x);
 		v = plane.z - floor(plane.z);
 	}
-	else if (fabs(normal.x) > EPSILON) //doublecheck this before eval!!
+	else if (fabs(normal.x) > EPSILON)
 	{
 		u = plane.z - floor(plane.z);
 		v = plane.y - floor(plane.y);
 	}
-	else if (fabs(normal.z) > EPSILON) //doublecheck this before eval!!
+	else if (fabs(normal.z) > EPSILON)
 	{
 		u = plane.x - floor(plane.x);
 		v = plane.y - floor(plane.y);
@@ -30,27 +30,16 @@ t_coord	spherical_map(t_coord point, t_float radius, t_coord normal)
 	t_float	v;
 	t_float	theta;
 	t_float	phi;
+	t_float	scale;
 
-	//if (fabs(normal.y) > EPSILON)
-	//{
-		theta = atan2(point.x, point.z);
-		phi = acos(point.y / radius);
-	//}
-	//else if (fabs(normal.x) > EPSILON)
-	//{
-	//	theta = atan2(point.y, point.z);
-	//	phi = acos(point.x / radius);
-	//}
-	//else if (fabs(normal.z) > EPSILON)
-	//{
-	//	theta = atan2(point.x, point.y);
-	//	phi = acos(point.z / radius);
-	//}
+	scale = 10;
+	theta = atan2(point.x, point.z);
+	phi = acos(point.y / radius);
 	u = theta / (2.0 * PI);
 	u = 1.0 - (u + 0.5);
 	v = 1 - phi / PI;
-	u *= 10;
-	v *= 10;
+	u *= scale;
+	v *= scale;
 
 	return (coord(u, v, 0));
 }
@@ -58,11 +47,15 @@ t_coord	spherical_map(t_coord point, t_float radius, t_coord normal)
 bool	checker_color(t_intersec *object)
 {
 	t_coord	uv;
+	t_coord	relative_point;
 
 	if (object->type == 'p')
 		uv = planar_map(object->point, object->normal);
 	else if (object->type == 's')
-		uv = spherical_map(object->point, object->self->sp.radius, object->normal);
+	{
+		relative_point = vec_sub(object->point, object->self->sp.center);
+		uv = spherical_map(relative_point, object->self->sp.radius, object->normal);
+	}
 	else
 		return (false);
 	if (((int)floor(uv.x * 2) % 2) == ((int)floor(uv.y * 2) % 2))
