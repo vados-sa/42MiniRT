a61de6b049d28e696b0c8e91d4e6512f9bfb506e
pebencze <petra.bncz@gmail.com>
2024-12-04 16:18:19 +0100

    solved multilight problem

--- incl/minirt.h
+++ incl/minirt.h
@@ -86,12 +86,12 @@ t_intersec	*cylinder_intersect(t_ray ray, t_object *obj);
 t_intersec	*intersect_cap(t_ray ray, t_object *obj, t_float t);
 t_intersec	*intersection(t_data *data, t_ray ray);
 t_intersec	*obj_intersection(t_data *data, t_ray ray, t_object *object);
-//t_float		find_t(t_ray ray, t_object *obj, char type);
 uint32_t	create_color(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
 
 /*light*/
 t_color		diffuse_light(t_l *l, t_intersec *hit_rec, t_data *data);
-bool		check_shadow(t_ray light, t_coord hit_rec_point, t_data *data);
+bool		check_shadow(t_ray light, t_coord point, t_data *data, \
+	t_coord normal);
 t_color		calculate_light(t_data *data, t_intersec *hit_rec, t_color color);
 
 /*hook functions*/
--- src/render/lights.c
+++ src/render/lights.c
@@ -10,12 +10,12 @@ t_color	diffuse_light(t_l *l, t_intersec *hit_rec, t_data *data)
 	bool	shadow;
 
 	diffuse = col(0, 0, 0, 0);
-	while (l && hit_rec)
+	while (l != NULL && hit_rec)
 	{
 		light.direction = vec_unit(vec_sub(l->point, hit_rec->point));
 		light.origin = l->point;
 		dot_product = fmax(vec_dot(hit_rec->normal, light.direction), EPSILON);
-		shadow = check_shadow(light, hit_rec->point, data);
+		shadow = check_shadow(light, hit_rec->point, data, hit_rec->normal);
 		if (dot_product > EPSILON && shadow == false)
 		{
 			temp_diffuse = \
@@ -23,6 +23,7 @@ t_color	diffuse_light(t_l *l, t_intersec *hit_rec, t_data *data)
 			diffuse = col_add(diffuse, temp_diffuse);
 		}
 		l = l->next;
+
 	}
 	return (diffuse);
 }
@@ -38,17 +39,17 @@ t_color	calculate_light(t_data *data, t_intersec *hit_rec, t_color color)
 	result.r = ((t_float)(ambient.r + diffuse.r) / 255.0) * color.r;
 	result.g = ((t_float)(ambient.g + diffuse.g) / 255.0) * color.g;
 	result.b = ((t_float)(ambient.b + diffuse.b) / 255.0) * color.b;
-	if (result.r > 255)
-		result.r = 255;
-	if (result.g > 255)
-		result.g = 255;
-	if (result.b > 255)
-		result.b = 255;
+	if (result.r > 255.0)
+		result.r = 255.0;
+	if (result.g > 255.0)
+		result.g = 255.0;
+	if (result.b > 255.0)
+		result.b = 255.0;
 	result.a = 255;
 	return (result);
 }
 
-bool	check_shadow(t_ray light, t_coord hit_rec_point, t_data *data)
+bool	check_shadow(t_ray light, t_coord point, t_data *data, t_coord normal)
 {
 	t_intersec	*shadow_hit;
 	t_object	*object;
@@ -56,16 +57,19 @@ bool	check_shadow(t_ray light, t_coord hit_rec_point, t_data *data)
 	t_float		max_len;
 
 	shadow_ray.direction = light.direction;
-	shadow_ray.origin = hit_rec_point;
+	shadow_ray.origin = vec_add(point, \
+		vec_mult(normal, EPSILON));
+
 	object = data->scene->objects;
 	max_len = vec_len(vec_sub(light.origin, shadow_ray.origin));
 	while (object)
 	{
 		shadow_hit = NULL;
 		shadow_hit = obj_intersection(data, shadow_ray, object);
-		object = object->next;
-		if (shadow_hit && shadow_hit->t > EPSILON && shadow_hit->t < max_len)
+		if (shadow_hit && shadow_hit->t > EPSILON
+			&& shadow_hit->t < max_len - EPSILON)
 			return (true);
+		object = object->next;
 	}
 	return (false);
 }
